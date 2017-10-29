f4f6a88549f577571b64f46146168f296414d50a
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2017-10-29 13:12:22 +0200

    Add FFI module for URLSearchParams, as DOM does not support them

---
+++ src/URLSearchParams.js
@@ -0,0 +1,8 @@
+"use strict";
+
+exports.get = function(paramName) {
+  return function() {
+    var url = new URL(window.location.toString());
+    return url.searchParams.get(paramName);
+  };
+};
