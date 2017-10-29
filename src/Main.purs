ab1ffcb5093da0fb3f42cc4522882227d84f79ae
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2017-10-29 17:56:07 +0200

    Wire gitlab ajax call into Main

--- src/Main.purs
+++ src/Main.purs
@@ -2,13 +2,31 @@ module Main where
 
 import Prelude
 
+import Control.Monad.Aff (Fiber, launchAff)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import DOM (DOM)
+import Gitlab (getProjects)
+import Network.HTTP.Affjax (AJAX)
 import URLSearchParams as URLParams
 
-main :: forall e. Eff (dom :: DOM, console :: CONSOLE | e) Unit
+type Main = forall e.
+            Eff ( ajax :: AJAX
+                , console :: CONSOLE
+                , dom :: DOM
+                | e)
+                (Fiber ( ajax :: AJAX
+                       , console :: CONSOLE
+                       , dom :: DOM
+                       | e)
+                       Unit)
+
+main :: Main
 main = do
-  token     <- URLParams.get "private_token"
-  gitlabUrl <- URLParams.get "gitlab_url"
-  log (token <> " " <> gitlabUrl)
+  token   <- URLParams.get "private_token"
+  baseUrl <- URLParams.get "gitlab_url"
+  log (baseUrl <> " " <> token)
+  launchAff $ do
+    projects <- getProjects baseUrl token
+    liftEff $ logShow projects
