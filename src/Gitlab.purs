172be12a6dc92cb3d5c117339c04323c200d4540
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2017-10-29 19:57:38 +0200

    Implement JSON reading for Gitlab Projects API

--- src/Gitlab.purs
+++ src/Gitlab.purs
@@ -1,17 +1,40 @@
 module Gitlab where
 
 import Prelude
-import Network.HTTP.Affjax (AJAX, get)
+
 import Control.Monad.Aff (Aff)
+import Data.Either (Either(..))
+import Network.HTTP.Affjax (AJAX, get)
+import Simple.JSON (class ReadForeign, class WriteForeign, readJSON)
+
 
 type BaseUrl = String
 type Token = String
 
-getProjects :: forall a. BaseUrl -> Token -> Aff (ajax :: AJAX | a) String
+newtype Id = Id Int
+derive newtype instance rfId :: ReadForeign Id
+derive newtype instance wfId :: WriteForeign Id
+
+newtype Name = Name String
+derive newtype instance rfName :: ReadForeign Name
+derive newtype instance wfName :: WriteForeign Name
+
+type Project =
+  { id :: Id
+  , name :: Name
+  }
+
+type Projects = Array Project
+
+
+getProjects :: forall a. BaseUrl -> Token -> Aff (ajax :: AJAX | a) Projects
 getProjects baseUrl token = do
   let url = baseUrl
             <> "/api/v4/projects?private_token="
             <> token
             <> "&simple=true&per_page=20&order_by=last_activity_at"
   projectsRes <- get url
-  pure $ projectsRes.response
+  let ps = case readJSON projectsRes.response of
+        Left e -> []
+        Right projects -> projects
+  pure ps
