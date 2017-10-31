8e27db8c0b564bda8fb3c94ce0c47113dad2df1b
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2017-10-31 20:44:10 +0200

    Remove newtypes for things returned by the API

--- src/Gitlab.purs
+++ src/Gitlab.purs
@@ -11,17 +11,9 @@ import Simple.JSON (class ReadForeign, class WriteForeign, readJSON)
 type BaseUrl = String
 type Token = String
 
-newtype Id = Id Int
-derive newtype instance rfId :: ReadForeign Id
-derive newtype instance wfId :: WriteForeign Id
-
-newtype Name = Name String
-derive newtype instance rfName :: ReadForeign Name
-derive newtype instance wfName :: WriteForeign Name
-
 type Project =
-  { id :: Id
-  , name :: Name
+  { id :: Int
+  , name :: String
   }
 
 type Projects = Array Project
