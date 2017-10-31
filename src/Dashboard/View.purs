66410d58c4ef3720b9dd3620a8c614a0109dc058
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2017-11-01 01:20:39 +0200

    Setup module Dashboard.View
    That module would contain functions that produce halogen's HTML given
    certain parameters. Most of them would be ported from corresponding
    javascript functions fairly directly.

---
+++ src/Dashboard/View.purs
@@ -0,0 +1,9 @@
+module Dashboard.View where
+
+import Prelude ((#), (<>), (<$>))
+import Halogen.HTML
+import Halogen.HTML as H
+import Halogen.HTML.Properties as P
+import Halogen.HTML.CSS (style) as P
+import CSS as CSS
+import CSS (px)
