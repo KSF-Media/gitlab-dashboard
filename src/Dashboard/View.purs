05a2ddd3477ea740cfd7dedaf6e353270f1e6414
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2017-11-07 16:16:20 +0200

    Revise the imports

--- src/Dashboard/View.purs
+++ src/Dashboard/View.purs
@@ -1,7 +1,7 @@
 module Dashboard.View where
 
 import Prelude
-import Halogen.HTML
+import Halogen.HTML (HTML)
 import Halogen.HTML as H
 import Halogen.HTML.Properties as P
 import Halogen.HTML.CSS (style) as P
