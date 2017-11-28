ceb8452cfc2487fa03dc95f1411162dddbe23558
Konstantin Zudov <co@zudov.me>
2017-11-28 02:44:39 +0200

    Move Icon stuff into it's own module

--- src/Dashboard/View.purs
+++ src/Dashboard/View.purs
@@ -1,23 +1,23 @@
 module Dashboard.View where
 
-import Dashboard.Model
-import Gitlab
 import Prelude
+
+import Gitlab
 
 import CSS (px, em)
 import CSS as CSS
 import CSS.TextAlign as CSS
-import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Show (genericShow)
 import Data.JSDate (JSDate)
 import Data.Newtype (unwrap)
-import Data.String as String
-import Data.Time.Duration (Milliseconds(..))
+import Data.Time.Duration (Milliseconds)
 import Halogen.HTML (HTML, ClassName(..))
 import Halogen.HTML as H
 import Halogen.HTML.CSS (style)
 import Halogen.HTML.Properties as P
 import Moment (formatMillis, fromNow)
+
+import Dashboard.Model (CommitRow, PipelineRow)
+import Dashboard.View.Icon (Icon(..), IconName(..), Modifier(..), fontAwesome)
 
 authorImage :: ∀ p i. String -> HTML p i
 authorImage url =
@@ -29,105 +29,21 @@ authorImage url =
         CSS.borderRadius (20.0 # px) (20.0 # px) (20.0 # px) (20.0 # px)
     ]
 
--- | TODO: Generate `purescript-fontawesome` library.
-data IconName
-  = Calendar
-  | CircleO
-  | ClockO
-  | Code
-  | CodeFork
-  | Stack
-  | DotCircleO
-  | UserCircleO
-  | Refresh
-  | QuestionCircleO
-  | CheckCircleO
-  | TimesCircleO
-  | StopCircleO
-  | ArrowCircleORight
-
-iconName :: IconName -> String
-iconName icon =
-  case icon of
-    Calendar          -> "calendar"
-    CircleO           -> "circle-o"
-    ClockO            -> "clock-o"
-    Code              -> "code"
-    CodeFork          -> "code-fork"
-    Stack             -> "stack"
-    DotCircleO        -> "dot-circle-o"
-    UserCircleO       -> "user-circle-o"
-    Refresh           -> "refresh"
-    QuestionCircleO   -> "question-circle-o"
-    CheckCircleO      -> "check-circle-o"
-    TimesCircleO      -> "times-circle-o"
-    StopCircleO       -> "stop-circle-o"
-    ArrowCircleORight -> "arrow-circle-o-right"
-
-data Modifier
-  = Stack2x
-  | Stack1x
-  | Inverse
-  | AlignMiddle
-  | Size2x
-  | Spin
-
-modifierClass :: Modifier -> ClassName
-modifierClass modifier =
-  ClassName
-    case modifier of
-      Stack2x -> "fa-stack-2x"
-      Stack1x -> "fa-stack-1x"
-      Inverse -> "fa-inverse"
-      AlignMiddle -> "align-middle"
-      Size2x  -> "fa-2x"
-      Spin    -> "fa-spin"
-
-data Icon
-  = Icon (Array Modifier) IconName
-  | IconStack (Array Icon)
-
-icon :: Array Modifier -> IconName -> Icon
-icon = Icon
-
-icon_ :: IconName -> Icon
-icon_ = Icon []
-
-stack :: Array Icon -> Icon
-stack = IconStack
-
-fontAwesome :: ∀ p i. Icon -> HTML p i
-fontAwesome (Icon modifiers icon) =
-  H.i
-    [ P.classes (fontAwesomeClasses icon <> map modifierClass modifiers)
-    , style do
-         CSS.margin (0.0 # px) (3.0 # px) (0.0 # px) (3.0 # px)
-    ]
-    [ ]
-fontAwesome (IconStack icons) =
-  H.span
-    [ P.classes (fontAwesomeClasses Stack) ]
-    (fontAwesome <$> icons)
-
-fontAwesomeClasses :: IconName -> Array ClassName
-fontAwesomeClasses icon =
-  ClassName <$> [ "fa", "fa-" <> iconName icon ]
-
 statusIcon :: ∀ p i. JobStatus -> HTML p i
 statusIcon status =
   fontAwesome
     case status of
-      JobRunning  -> stack
-         [ icon [ Stack2x ] CircleO
-         , icon [ Stack1x, Inverse, Spin ] Refresh
+      JobRunning  -> IconStack
+         [ Icon [ Stack2x ] CircleO
+         , Icon [ Stack1x, Inverse, Spin ] Refresh
          ]
-      JobCreated  -> icon [ Size2x, AlignMiddle ] DotCircleO
-      JobManual   -> icon [ Size2x, AlignMiddle ] UserCircleO
-      JobPending  -> icon [ Size2x, AlignMiddle ] QuestionCircleO
-      JobSuccess  -> icon [ Size2x, AlignMiddle ] CheckCircleO
-      JobFailed   -> icon [ Size2x, AlignMiddle ] TimesCircleO
-      JobCanceled -> icon [ Size2x, AlignMiddle ] StopCircleO
-      JobSkipped  -> icon [ Size2x, AlignMiddle ] ArrowCircleORight
+      JobCreated  -> Icon [ Size2x, AlignMiddle ] DotCircleO
+      JobManual   -> Icon [ Size2x, AlignMiddle ] UserCircleO
+      JobPending  -> Icon [ Size2x, AlignMiddle ] QuestionCircleO
+      JobSuccess  -> Icon [ Size2x, AlignMiddle ] CheckCircleO
+      JobFailed   -> Icon [ Size2x, AlignMiddle ] TimesCircleO
+      JobCanceled -> Icon [ Size2x, AlignMiddle ] StopCircleO
+      JobSkipped  -> Icon [ Size2x, AlignMiddle ] ArrowCircleORight  
 
 formatStatus :: ∀ p a. PipelineRow -> HTML p a
 formatStatus { id: PipelineId id, status } =
@@ -150,10 +66,10 @@ formatCommit { authorImg
     [ ]
     [ authorImage authorImg
     , divider
-    , fontAwesome $ icon_ CodeFork
+    , fontAwesome $ Icon [] CodeFork
     , H.b_ [ H.text (" " <> branch) ]
     , divider
-    , fontAwesome $ icon_ Code
+    , fontAwesome $ Icon [] Code
     , H.text (" " <> hash)
     , H.br_
     , H.div
@@ -175,10 +91,10 @@ formatTimes { when, duration } =
         CSS.textAlign CSS.rightTextAlign
         CSS.paddingRight (2.0 # em)
     ]
-    [ fontAwesome $ icon_ ClockO
+    [ fontAwesome $ Icon [] ClockO
     , H.text (" " <> formatMillis duration)
     , H.br_
-    , fontAwesome $ icon_ Calendar
+    , fontAwesome $ Icon [] Calendar
     , H.text (" " <> fromNow when)
     ]
 
